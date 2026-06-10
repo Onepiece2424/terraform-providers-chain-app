edbd12932858e12a785e088381b5e45f2aedd4a3
rioto.nagano <kingdom950927@gmail.com>
2026-06-10 16:26:37 +0900

    add: 子モジュールにも同じバージョンを使用するように設定

--- aws/main.tf
+++ aws/main.tf
@@ -13,3 +13,14 @@ provider "aws" {
 resource "aws_s3_bucket" "example" {
   bucket_prefix = "terraform-provider-example-"
 }
+
+module "website" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 5.2.0"
+
+  bucket_prefix = "terraform-provider-website-"
+
+  tags = {
+    Module = "s3-bucket"
+  }
+}
