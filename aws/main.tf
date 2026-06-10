fb5cd74051730600aff71eb101cdc64e4952350f
rioto.nagano <kingdom950927@gmail.com>
2026-06-10 15:58:00 +0900

    add: デフォルトタグの設定

--- aws/main.tf
+++ aws/main.tf
@@ -1,6 +1,14 @@
 provider "aws" {
   region = "us-west-2"
+
+  default_tags {
+    tags = {
+      Environment = "tutorial"
+      Project     = "terraform-configure-providers"
+    }
+  }
 }
+
 
 resource "aws_s3_bucket" "example" {
   bucket_prefix = "terraform-provider-example-"
